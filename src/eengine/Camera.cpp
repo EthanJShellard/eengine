fb6743d6ae925e4d324e2072b9dc9c3c13331af7
Ethan Shellard <ethanshellard@gmail.com>
2022-10-26 15:44:16 +0100

    Add doxygen comments to Camera

--- src/eengine/Camera.cpp
+++ src/eengine/Camera.cpp
@@ -3,19 +3,19 @@
 
 namespace eengine 
 {
-	Camera::Camera(float _vertFov, float _near, float _far, shared<Transform> _transform)
+	Camera::Camera(float _vertFov, float _near, float _far) :
+		m_vFOV(_vertFov),
+		m_near(_near),
+		m_far(_far),
+		m_transform(std::make_shared<Transform>())
 	{
-		m_vFOV = _vertFov;
-		m_near = _near;
-		m_far = _far;
-		m_transform = _transform;
 	}
 
-	Camera::Camera()
+	Camera::Camera():
+		m_vFOV(45.0f),
+		m_near(0.1f),
+		m_far(1000.0f),
+		m_transform(std::make_shared<Transform>())
 	{
-		m_vFOV = 45.0f;
-		m_near = 0.1f;
-		m_far = 100.0f;
-		m_transform = std::make_shared<Transform>();
 	}
 }
--- src/eengine/Camera.h
+++ src/eengine/Camera.h
@@ -5,15 +5,27 @@ namespace eengine
 	// Engine forward declarations
 	class Transform;
 
+	/// @brief A simple wrapper class for Camera data.
 	class Camera
 	{
 	public:
+		/// @brief Vertical field of view, degrees.
 		float m_vFOV;
+		/// @brief Distance to the near plane.
 		float m_near;
+		/// @brief Distance to the far plane.
 		float m_far;
+		/// @brief This camera's transform.
 		shared<Transform> m_transform;
 
-		Camera(float _vFOV, float _near, float _far, shared<Transform> _tf);
+		/// @brief Detailed constructor for a Camera.
+		/// @param _vFOV Vertical field of view, degrees.
+		/// @param _near Distance to the near plane.
+		/// @param _far Distance to the far plane.
+		Camera(float _vFOV, float _near, float _far);
+
+		/// @brief Default Camera constructor.
+		/// @details Vertical FOV defaults to 45 degrees, near plane distance defaults to 0.1f and the far plane distance defaults to 1000.0f.
 		Camera();
 	};
 }
