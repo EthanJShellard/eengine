89f9474b133eff43fae23b8a2c9729d8ef63f123
Ethan Shellard <ethanshellard@gmail.com>
2022-10-26 15:50:07 +0100

    Add doxygen comments to Environment

--- src/eengine/Environment.h
+++ src/eengine/Environment.h
@@ -8,21 +8,25 @@ namespace eengine
 	// Engine forward declarations
 	class Core;
 
+	/// @brief Class which provides information about the environment surrounding the engine.
 	class Environment 
 	{
 		friend class Core;
 
 	private:
 		std::chrono::time_point<std::chrono::steady_clock, std::chrono::duration<float>> m_lastTime;
-
 		float m_deltaTime;
-		
 		std::string m_projectWorkingDirectory;
 
 		Environment();
 		void OnFrameStart();
 	public:
+		/// @brief Get the time between the start of this frame and the start of the last frame.
+		/// @return The time between the start of this frame and the start of the last frame in seconds.
 		float GetDeltaTime();
+
+		/// @brief Get the path to the project working directory, where the executable can be found.
+		/// @return The path to the project working directory as an std::string.
 		std::string GetProjectWorkingDirectory();
 	};
 }
